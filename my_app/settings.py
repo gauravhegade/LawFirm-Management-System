992de16e1927abc388a41fa3d4e38e2ba9a58575
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2024-10-03 10:32:31 +0530

    Default

--- my_app/settings.py
+++ my_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^y1p205fuz*!*rh=x5@eo-&o5^musb0zbw7e396ji@y7nx!la%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
